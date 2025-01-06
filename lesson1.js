0136676d2884a2a322087c635f81b79d35cad826
TheRealPharahFresh <therealpharaohfresh@gmail.com>
2025-01-06 14:55:22 -0500

    first commit

---
+++ lesson1.js
@@ -0,0 +1,19 @@
+let name1 = "Don";
+let age = 23;
+let grade = 10;
+let attendance = true;
+
+console.log("Name:", name1);
+console.log("Age:", age);
+console.log("Grade:", grade);
+console.log("Attendance:", attendance);
+
+let studentName = "Alice";
+let studentAge = 15;
+let studentGrade = 11;
+let isPresent = true;
+
+console.log("Name:", studentName);
+console.log("Student Age:", studentAge);
+console.log("Student Grade:", studentGrade);
+console.log("Student Is Present:", isPresent);
