b323e554d2ad5ac89b0bb8e63f3b63c8a9533c7a
TheRealPharahFresh <therealpharaohfresh@gmail.com>
2025-01-08 17:49:26 -0500

    second commit

--- lesson1.js
+++ lesson1.js
@@ -17,3 +17,38 @@ console.log("Name:", studentName);
 console.log("Student Age:", studentAge);
 console.log("Student Grade:", studentGrade);
 console.log("Student Is Present:", isPresent);
+
+let w = 40;
+let z = 30;
+let x = 20;
+let y = 10;
+let sum = x + y;
+let difference = x - y;
+let product = x * y;
+let quotient = x / y;
+
+console.log("Sum: ", sum);
+console.log("Difference: ", difference);
+console.log("Product:", product);
+console.log("Quotient:", quotient);
+
+x += 5;
+console.log("Update Num1:", x);
+y -= 10;
+console.log("Update Num2:", y);
+
+let isEqualTo = x === y;
+let isGreaterThan = x > y;
+let isLesserThan = y < x;
+
+console.log("Is num1 equal to num 2? ", isEqualTo);
+console.log("Is num1 greater than num2? ", isGreaterThan);
+console.log("Is num2 lesser than num 1? ", isLesserThan);
+
+let isPositive = w > 0 && z > 0; 
+let isEven = w % 2 === 0 || z % 2 === 0; 
+
+console.log("Are both numbers positive? ", isPositive);
+console.log("Is Atleast one number even? ", isEven);
+
+
